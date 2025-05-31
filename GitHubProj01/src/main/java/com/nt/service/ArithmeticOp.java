539158fce1cf6c4db5efe69a870352d659157f38
cheta <cheta@Chetan>
2025-05-31 22:46:52 +0530

    initial project directory by tl

---
+++ GitHubProj01/src/main/java/com/nt/service/ArithmeticOp.java
@@ -0,0 +1,13 @@
+package com.nt.service;
+
+public class ArithmeticOp {
+	
+	public int sum(int x,int y)
+	{
+		return x+y;
+	}
+	
+	public static void main(String[] args) {
+		ArithmeticOp ap = new ArithmeticOp();
+	}
+}
---
+++ GitHubProj01/src/test/java/com/nt/test/ArithmeticOpTest.java
@@ -0,0 +1,31 @@
+package com.nt.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import com.nt.service.ArithmeticOp;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class ArithmeticOpTest {
+
+	private static ArithmeticOp op;
+	
+	@BeforeAll
+	public static void tearOnce()
+	{
+		op = new ArithmeticOp();
+	}
+	
+	@Test
+	public void testSumWithPositive()
+	{
+		int a=10,b=20;
+		int expacted=30;
+		int actual=op.sum(a,b);
+		assertEquals(expacted,actual);	
+	}
+}
